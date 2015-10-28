1499e53f5a2992adaba4b8a87b8be47a87c14de7
Dmitriy <reven911@gmail.com>
2015-10-28 16:49:06 +0300

    add classes main and printer

---
+++ src/main/java/com/jet/main/Main.java
@@ -0,0 +1,10 @@
+package com.jet.main;
+
+import com.jet.presentation.Printer;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Printer.print("Hello World!");
+	}
+}
---
+++ src/main/java/com/jet/presentation/Printer.java
@@ -0,0 +1,8 @@
+package com.jet.presentation;
+
+public class Printer {
+	
+	public static void print(String str) {
+		System.out.println(str);
+	}
+}
