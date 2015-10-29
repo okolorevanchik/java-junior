a93a8ed3be1a02d023be5cc2c20072444083ff7f
Dmitriy <reven911@gmail.com>
2015-10-29 16:14:57 +0300

    Adding comments.

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -2,18 +2,28 @@ package com.acme.edu;
 
 public class Logger {
 
+    private static final String PRIMITIVE = "primitive: ";
+
+    /**
+     * Prints an integer and then terminate the line.
+     * @param message The int to be printed.
+     */
     public static void log(int message) {
-        consoleWriter("primitive: " + message);
+        consoleWriter(PRIMITIVE + message);
     }
 
-    public static void log(byte message) {
-        consoleWriter("primitive: " + message);
-    }
-
+    /**
+     * Prints an boolean and then terminate the line.
+     * @param message The boolean to be printed.
+     */
     public static void log(boolean message) {
-        consoleWriter("primitive: " + message);
+        consoleWriter(PRIMITIVE + message);
     }
 
+    /**
+     * Prints an character and then terminate the line.
+     * @param message The char to be printed.
+     */
     public static void log(char message) {
         consoleWriter("char: " + message);
     }
