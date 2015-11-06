85f4738ea9352ddce39f662ea31c926d7518eb77
Dmitriy <reven911@gmail.com>
2015-11-06 11:05:00 +0300

    Add test for checked coll method flush to class State when current state not equal new state.

--- src/test/java/com/acme/edu/unit/ManagedStateTest.java
+++ src/test/java/com/acme/edu/unit/ManagedStateTest.java
@@ -5,10 +5,14 @@ import com.acme.edu.exceptions.GetStateException;
 import com.acme.edu.exceptions.IncorrectArgumentsConstructorException;
 import com.acme.edu.printers.Printable;
 import com.acme.edu.states.ManagedState;
+import com.acme.edu.states.State;
+import com.acme.edu.states.UnbufferedState;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 public class ManagedStateTest {
 
@@ -38,6 +42,14 @@ public class ManagedStateTest {
         managedState.getDefaultState(null);
     }
 
+    @Test
+    public void shouldCollMethodFlushFromCurrentStateWhenCurrentStateNotEqualNewState() throws Exception {
+        ManagedState managedState = new ManagedState(printable, decorate);
+        State currentState = mock(UnbufferedState.class);
 
+        managedState.getNumberState(currentState);
+
+        verify(currentState, times(1)).flush();
+    }
 
 }
