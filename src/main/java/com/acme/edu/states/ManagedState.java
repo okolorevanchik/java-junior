54805bcf11c1936b89b51c3d0911e67a081d6636
Dmitriy <reven911@gmail.com>
2015-11-06 12:16:16 +0300

    Refactoring ManagedStateTest

--- src/main/java/com/acme/edu/states/ManagedState.java
+++ src/main/java/com/acme/edu/states/ManagedState.java
@@ -1,9 +1,8 @@
 package com.acme.edu.states;
 
 import com.acme.edu.Decorate;
-import com.acme.edu.exceptions.GetStateException;
 import com.acme.edu.exceptions.IncorrectArgumentsConstructorException;
-import com.acme.edu.exceptions.IncorrectInputsParametersMethodException;
+import com.acme.edu.exceptions.PrintDataException;
 import com.acme.edu.printers.Printable;
 
 public class ManagedState {
@@ -21,25 +20,23 @@ public class ManagedState {
         this.defaultState = new UnbufferedState(printable, decorate);
     }
 
-    public State getIntBufferState(State currentState) throws GetStateException {
+    public State getIntBufferState(State currentState) throws PrintDataException {
         printBuffer(currentState, numberState);
         return numberState;
     }
 
-    public State getStringBufferState(State currentState) throws GetStateException {
+    public State getStringBufferState(State currentState) throws PrintDataException {
         printBuffer(currentState, stringState);
         return stringState;
     }
 
-    public State getUnbufferedState(State currentState) throws GetStateException {
+    public State getUnbufferedState(State currentState) throws PrintDataException {
         printBuffer(currentState, defaultState);
         return defaultState;
     }
 
-    private void printBuffer(State currentState, State checkedState) throws GetStateException {
-        if (currentState == null) {
-            throw new IncorrectInputsParametersMethodException("Method parameter can not be null.");
-        } else if (currentState != checkedState) {
+    private void printBuffer(State currentState, State checkedState) throws PrintDataException {
+        if (currentState != null && currentState != checkedState) {
             currentState.flush();
         }
     }
