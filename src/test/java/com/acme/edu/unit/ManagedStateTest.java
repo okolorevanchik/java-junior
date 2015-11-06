83bc51ad32ce68fb63577a9773b311fef0095245
Dmitriy <reven911@gmail.com>
2015-11-06 11:46:17 +0300

    Add tests for checked creation actual objects.

--- src/test/java/com/acme/edu/unit/ManagedStateTest.java
+++ src/test/java/com/acme/edu/unit/ManagedStateTest.java
@@ -4,24 +4,26 @@ import com.acme.edu.Decorate;
 import com.acme.edu.exceptions.GetStateException;
 import com.acme.edu.exceptions.IncorrectArgumentsConstructorException;
 import com.acme.edu.printers.Printable;
-import com.acme.edu.states.ManagedState;
-import com.acme.edu.states.State;
+import com.acme.edu.states.*;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
 
 public class ManagedStateTest {
 
-    private static final String PRIMITIVE_PREFIX = "primitive: %s";
-
     private Printable printable;
     private Decorate decorate;
+    private State currentState;
+    private ManagedState managedState;
 
     @Before
-    public void setUp() {
+    public void setUp() throws Exception {
         printable = mock(Printable.class);
         decorate = mock(Decorate.class);
+        currentState = mock(State.class);
+        managedState = new ManagedState(printable, decorate);
     }
 
     @Test(expected = IncorrectArgumentsConstructorException.class)
@@ -36,16 +38,11 @@ public class ManagedStateTest {
 
     @Test(expected = GetStateException.class)
     public void shouldThrowGetStateException() throws Exception {
-        ManagedState managedState = new ManagedState(printable, decorate);
-
         managedState.getDefaultState(null);
     }
 
     @Test
     public void shouldCallMethodFlushFromCurrentStateWhenCurrentStateNotEqualNewState() throws Exception {
-        ManagedState managedState = new ManagedState(printable, decorate);
-        State currentState = mock(State.class);
-
         managedState.getNumberState(currentState);
 
         verify(currentState, times(1)).flush();
@@ -53,9 +50,6 @@ public class ManagedStateTest {
 
     @Test
     public void shouldNotCallMethodFlushFromCurrentStateWhenCurrentStateEqualNewState() throws Exception {
-        ManagedState managedState = new ManagedState(printable, decorate);
-        State currentState = mock(State.class);
-
         State newCurrentState = managedState.getDefaultState(currentState);
         State newCurrentStateMock = mock(newCurrentState.getClass());
         managedState.getDefaultState(newCurrentState);
@@ -63,4 +57,24 @@ public class ManagedStateTest {
         verify(newCurrentStateMock, times(0)).flush();
     }
 
+    @Test
+    public void shouldGetIntBufferState() throws Exception {
+        State actualState = managedState.getNumberState(currentState);
+
+        assertTrue(actualState instanceof IntBufferState);
+    }
+
+    @Test
+    public void shouldGetStringBufferState() throws Exception {
+        State actualState = managedState.getStringState(currentState);
+
+        assertTrue(actualState instanceof StringBufferState);
+    }
+
+    @Test
+    public void shouldGetUnbufferedState() throws Exception {
+        State actualState = managedState.getDefaultState(currentState);
+
+        assertTrue(actualState instanceof UnbufferedState);
+    }
 }
